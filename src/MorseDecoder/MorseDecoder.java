562446f63922d493552161a5cd14e2c28eb202a2
Trevin Hofmann <trevinhofmann@gmail.com>
2014-05-02 09:33:48 -0500

    First commit

---
+++ src/MorseDecoder/MorseDecoder.java
@@ -0,0 +1,18 @@
+package MorseDecoder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hofmannt
+ * Date: 5/2/14
+ * Time: 9:21 AM
+ * This class loads the morse code information, gets the files for input and output from the user,
+ * and converts morse code into plain text.
+ */
+
+public class MorseDecoder {
+
+	public static void main(String[] args){
+
+	}
+
+}
---
+++ src/MorseDecoder/MorseTree.java
@@ -0,0 +1,18 @@
+package MorseDecoder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hofmannt
+ * Date: 5/2/14
+ * Time: 9:21 AM
+ * This class is a Tree for converting morse code to plain characters. It contains each character, and
+ * is traversed by going left for a dot and right for a dash.
+ */
+
+public class MorseTree<E> {
+
+	public MorseTree(){
+
+	}
+
+}
