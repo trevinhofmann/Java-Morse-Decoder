2e3d32d91dd937f5f74b43dc7fc20a9a185696eb
Trevin Hofmann <trevinhofmann@gmail.com>
2014-05-04 19:36:13 -0500

    Create inner Node class

--- src/MorseDecoder/MorseTree.java
+++ src/MorseDecoder/MorseTree.java
@@ -11,8 +11,44 @@ package MorseDecoder;
 
 public class MorseTree<E> {
 
-	public MorseTree(){
+	/**
+	 * The tree's root Node
+	 */
+	private Node root;
 
+	private class Node<E>{
+
+		/**
+		 * Left child
+		 */
+		private Node left;
+
+		/**
+		 * Right child
+		 */
+		private Node right;
+
+		/**
+		 * Value of the Node
+		 */
+		private E value;
+
+		/**
+		 * Construct a Node
+		 * @param left left child
+		 * @param right right child
+		 * @param value stored value
+		 */
+		private Node(Node left, Node right, E value){
+			this.left = left;
+			this.right = right;
+			this.value = value;
+		}
+
+	}
+
+	public MorseTree(){
+		root = new Node(null, null, null);
 	}
 
 }
