b8307802084779d092c02856d5b10a023f6a65d8
Trevin Hofmann <trevinhofmann@gmail.com>
2014-05-04 20:15:58 -0500

    Add loadTree method

--- src/MorseDecoder/MorseDecoder.java
+++ src/MorseDecoder/MorseDecoder.java
@@ -1,5 +1,9 @@
 package MorseDecoder;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * User: hofmannt
@@ -11,8 +15,34 @@ package MorseDecoder;
 
 public class MorseDecoder {
 
-	public static void main(String[] args){
+	/**
+	 * Main method of the program
+	 * Loads the morse tree, reads encoded input, and writes decoded output
+	 * @param args
+	 */
+	public static void main(String[] args) throws FileNotFoundException{
+		MorseTree<String> morseTree = loadTree(new File("morsecode.txt"));
+	}
 
+	private static MorseTree<String> loadTree(File morse) throws FileNotFoundException{
+		MorseTree<String> morseTree = new MorseTree<String>();
+		Scanner scanner = null;
+		try{
+			scanner = new Scanner(morse);
+			String line;
+			while (scanner.hasNextLine()){
+				line = scanner.nextLine();
+				String symbol = line.split("\t| ")[0];
+				String code = line.split("\t| ")[1];
+				morseTree.add(symbol, code);
+			}
+		}
+		finally {
+			if (scanner != null) {
+				scanner.close();
+			}
+		}
+		return morseTree;
 	}
 
 }
